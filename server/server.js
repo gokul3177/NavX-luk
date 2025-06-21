bb236b890f75723a78bf50d59cc349d8b99bb3bc
gokul3177 <gokulakumaran3281@gmail.com>
2025-06-21 15:42:09 +0530

    🚀 Fix CORS and connect to Railway MySQL

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const db = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 4000,
 });
 
 // Connect
